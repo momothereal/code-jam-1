c011b73cd2357dc608793a88475028784fb26b88
momothereal <momothereal.mc@gmail.com>
2018-03-23 23:14:51 -0400

    CGI sneks using perlin noise

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -1,9 +1,15 @@
 # coding=utf-8
+import io
 import logging
+import math
+import random
 
 import discord
+from PIL import Image
+from PIL.ImageDraw import ImageDraw
 from discord.ext.commands import AutoShardedBot, Context, command
 
+import bot.sneks.perlin as perlin
 from bot.sneks.sneks import Embeddable, SnakeDef, scrape_itis
 
 log = logging.getLogger(__name__)
@@ -66,6 +72,73 @@ class Snakes:
         embed = data.as_embed()
         log.debug("Sending embed: " + str(data.__dict__))
         await channel.send(embed=embed)
+
+    @command(name="snakes.draw()", aliases=["snakes.draw"])
+    async def draw(self, ctx: Context):
+        stream = self.generate_snake_image()
+        file = discord.File(stream, filename='snek.png')
+        await ctx.send(file=file)
+        pass
+
+    def generate_snake_image(self) -> bytes:
+        """
+        Generate a CGI snek using perlin noise
+        :return: the binary data of the PNG image
+        """
+        fac = perlin.PerlinNoiseFactory(dimension=1, octaves=2)
+        img_size = 200
+        margins = 50
+        start_x = random.randint(margins, img_size - margins)
+        start_y = random.randint(margins, img_size - margins)
+        points = [(start_x, start_y)]
+        snake_length = 12
+        snake_color = 0x15c7ea
+        text_color = 0xf2ea15
+        background_color = 0x0
+
+        for i in range(0, snake_length):
+            angle = math.radians(fac.get_plain_noise((1 / (snake_length + 1)) * (i + 1)) * 360)
+            curr_point = points[i]
+            segment_length = random.randint(15, 20)
+            next_x = curr_point[0] + segment_length * math.cos(angle)
+            next_y = curr_point[1] + segment_length * math.sin(angle)
+            points.append((next_x, next_y))
+
+        # normalize bounds
+        min_dimensions = [start_x, start_y]
+        max_dimensions = [start_x, start_y]
+        for p in points:
+            if p[0] < min_dimensions[0]:
+                min_dimensions[0] = p[0]
+            if p[0] > max_dimensions[0]:
+                max_dimensions[0] = p[0]
+            if p[1] < min_dimensions[1]:
+                min_dimensions[1] = p[1]
+            if p[1] > max_dimensions[1]:
+                max_dimensions[1] = p[1]
+
+        # shift towards middle
+        dimension_range = (max_dimensions[0] - min_dimensions[0], max_dimensions[1] - min_dimensions[1])
+        shift = (
+            img_size / 2 - (dimension_range[0] / 2 + min_dimensions[0]),
+            img_size / 2 - (dimension_range[1] / 2 + min_dimensions[1])
+        )
+
+        img = Image.new(mode='RGB', size=(img_size, img_size), color=background_color)
+        draw = ImageDraw(img)
+        for i in range(1, len(points)):
+            p = points[i]
+            prev = points[i - 1]
+            draw.line(
+                (shift[0] + prev[0], shift[1] + prev[1], shift[0] + p[0], shift[1] + p[1]),
+                width=8,
+                fill=snake_color
+            )
+        draw.multiline_text((img_size - margins, img_size - margins), text="snek\nit\nup", fill=text_color)
+        del draw
+        stream = io.BytesIO()
+        img.save(stream, format='PNG')
+        return stream.getvalue()
 
 
 def setup(bot):
