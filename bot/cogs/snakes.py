3adbc50bf20cd1ee7e14a53019ce2eb036996c57
Kel <kellrelu@gmail.com>
2018-03-23 23:41:32 -0700

    fixed snakeme, added hatching

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -6,6 +6,9 @@ from discord.ext.commands import AutoShardedBot, Context, command
 
 from bot.sneks.sneks import Embeddable, SnakeDef, scrape_itis, snakify
 from pymarkovchain import MarkovChain
+from bot.sneks.hatching import hatching,hatching_snakes
+import asyncio
+import random
 
 log = logging.getLogger(__name__)
 
@@ -76,15 +79,20 @@ class Snakes:
 
     @command(name="snakes.snakeme()",aliases=["snakes.snakeme","snakeme"])
     async def snakeme(self, ctx: Context):
-        # takes your last messages, trains a simple markov chain generator on what you've said, snakifies your response
-        author = ctx.message.author if(len(ctx.message.mentions) == 0) else ctx.message.mentions[0]
+        """
+        How would I talk if I were a snake? 
+        :param ctx: context
+        :return: you, snakified based on your Discord message history
+        """
+        mentions = list(filter(lambda m:m.id != self.bot.user.id,ctx.message.mentions))
+        author = ctx.message.author if(len(mentions) == 0) else ctx.message.mentions[0]
         channel : discord.TextChannel = ctx.channel
 
         channels = [ channel for channel in ctx.message.guild.channels if isinstance(channel,discord.TextChannel) ]
-        channels_messages = [ await channel.history(limit=1000).flatten() for channel in channels]
+        channels_messages = [ await channel.history(limit=10000).flatten() for channel in channels]
         msgs = [msg for channel_messages in channels_messages for msg in channel_messages][:MSG_MAX]
 
-        my_msgs = list(filter(lambda msg: msg.author == author, msgs))
+        my_msgs = list(filter(lambda msg: msg.author.id == author.id, msgs))
         my_msgs_content = "\n".join(list(map(lambda x:x.content, my_msgs)))
 
         mc = MarkovChain()
@@ -92,9 +100,39 @@ class Snakes:
         sentence = mc.generateString()
 
         snakeme = discord.Embed()
-        snakeme.set_author(name="{}#{} Snake".format(author.name,author.discriminator), icon_url = "https://cdn.discordapp.com/avatars/{}/{}".format(author.id,author.avatar))
+        snakeme.set_author(name="{}#{}".format(author.name,author.discriminator), icon_url = "https://cdn.discordapp.com/avatars/{}/{}".format(author.id,author.avatar) if author.avatar is not None else "https://img00.deviantart.net/eee3/i/2017/168/3/4/discord__app__avatar_rev1_by_nodeviantarthere-dbd2tp9.png")
         snakeme.description = "*{}*".format(snakify(sentence) if sentence is not None else ":question: Not enough messages")
-        await channel.send(snakeme)
+        await channel.send(embed=snakeme)
+    
+
+    @command(name="snakes.hatch()",aliases=["snakes.hatch","hatch"])
+    async def hatch(self,ctx: Context):
+        """ 
+        Hatches your personal snake
+        :param ctx: context
+        :return: baby snake
+        """
+        channel: discord.TextChannel = ctx.channel
+
+        my_snake = list(hatching_snakes.keys())[random.randint(0,3)]
+        my_snake_img = hatching_snakes[my_snake]
+        print(my_snake_img)
+
+        m = await channel.send(embed=discord.Embed(description="Hatching your snake :snake:..."))
+        await asyncio.sleep(1)
+
+        for i in range(len(hatching)):
+            hatch_embed = discord.Embed(description = hatching[i])
+            await m.edit(embed = hatch_embed)
+            await asyncio.sleep(1)
+        # await m.edit(embed = discord.Embed().set_thumbnail(url="https://i.imgur.com/5QHH4If.jpg"))
+        await asyncio.sleep(1)
+        await m.delete()
+
+        my_snake_embed = discord.Embed(description=":tada: Congrats!! You hatched: **{}**".format(my_snake))
+        my_snake_embed.set_thumbnail(url=my_snake_img)
+        my_snake_embed.set_footer(text=" Owner: {}#{}".format(ctx.message.author.name,ctx.message.author.discriminator))
+        await channel.send(embed=my_snake_embed)
         
 
 def setup(bot):
